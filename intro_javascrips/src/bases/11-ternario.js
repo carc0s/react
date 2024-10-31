f1f64177641cf3094641fba1d5bbbf6fc05cd5f0
carcos_b <carlosbe@fpune.edu.py>
2024-10-31 11:27:20 -0300

    x

---
+++ intro_javascrips/src/bases/11-ternario.js
@@ -0,0 +1,13 @@
+
+//operador ternario
+
+const edad = 18;
+
+const mensaje = edad >= 18 ? 'Eres mayor de edad' : 'Eres menor de edad';
+
+console.log(mensaje); // Eres mayor de edad
+//ternario mas corto
+
+const mensaje2 = edad >= 18 && 'Eres mayor de edad';
+
+console.log(mensaje2); // Eres mayor de edad
